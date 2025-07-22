7362faf79cb5f2c015cb136c12cbd8c458cc7ae9
shivang-jnv <skjnvspn@gmail.com>
2025-07-22 11:08:28 +0530

    updated schema.prisma

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,13 +1,34 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
+app.use(express.json())
 
 //password logic
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
+  const body = req.body;
 
-  
+  //store in db a new trigger
+  await client.$transaction(async (tx:any) => {
+    const run = await client.zapRun.create({
+      data:{
+        zapId: zapId,
+        metadata: body
+      }
+    });
 
+    await client.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id
+      }
+    });
+
+  })
 })
+
+app.listen(3000)
